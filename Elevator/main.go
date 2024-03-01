f2946c0f9d7180b64bc829a1b12f4a51b137d7b6
Kien Ninh Do <kiennd@stud.ntnu.no>
2024-03-01 15:07:28 +0100

    Heis 1.mars

---
+++ Elevator/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"Driver-go/elevio"
+	"fmt"
+)
+
+func main() {
+
+	// Given main function for going up and down and registering floors
+	numFloors := 4
+
+	elevio.Init("localhost:15657", numFloors)
+
+	// Choosing direction of the elevator
+	var d elevio.MotorDirection = elevio.MD_Up // Instead of up, we have to make a function that vhooses the direction of the elevator based on the order
+	//elevio.SetMotorDirection(d)
+
+	// Channels for all the different inputs
+	drv_buttons := make(chan elevio.ButtonEvent)
+	drv_floors := make(chan int)
+	drv_obstr := make(chan bool)
+	drv_stop := make(chan bool)
+
+	// Checks the state of the different input-channels
+	go elevio.PollButtons(drv_buttons)
+	go elevio.PollFloorSensor(drv_floors)
+	go elevio.PollObstructionSwitch(drv_obstr)
+	go elevio.PollStopButton(drv_stop)
+
+	// Infinite loop that checks the state of the different input-channels and does something every time it gets a signal
+	for {
+		select {
+		case a := <-drv_buttons:
+			fmt.Printf("%+v\n", a)
+			elevio.SetButtonLamp(a.Button, a.Floor, true)
+
+		case a := <-drv_floors:
+			fmt.Printf("%+v\n", a)
+			if a == numFloors-1 {
+				d = elevio.MD_Down
+			} else if a == 0 {
+				d = elevio.MD_Up
+			}
+			elevio.SetMotorDirection(d)
+
+		case a := <-drv_obstr:
+			fmt.Printf("%+v\n", a)
+			if a {
+				elevio.SetMotorDirection(elevio.MD_Stop)
+			} else {
+				elevio.SetMotorDirection(d)
+			}
+
+		case a := <-drv_stop:
+			fmt.Printf("%+v\n", a)
+			for f := 0; f < numFloors; f++ {
+				for b := elevio.ButtonType(0); b < 3; b++ {
+					elevio.SetButtonLamp(b, f, false)
+				}
+			}
+		}
+	}
+}
