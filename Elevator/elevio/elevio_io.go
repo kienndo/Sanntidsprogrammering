15f5aaa52d20fca4af5065115a6cfde10be937f8
Kien Ninh Do <kiennd@stud.ntnu.no>
2024-03-01 16:44:54 +0100

    Heis 1.mars

--- Elevator/elevio/elevio_io.go
+++ Elevator/elevio/elevio_io.go
@@ -1,19 +1,22 @@
 package elevio
 
-import "time"
-import "sync"
-import "net"
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"sync"
+	"time"
+)
 
 const _pollRate = 20 * time.Millisecond
 
-var _initialized    bool = false
-var _numFloors      int = 4
-var _mtx            sync.Mutex
-var _conn           net.Conn
+var _initialized bool = false
+var _numFloors int = 4
+var _mtx sync.Mutex
+var _conn net.Conn
 
 // Enum for the direction of the elevator
 type MotorDirection int
+
 const (
 	MD_Up   MotorDirection = 1
 	MD_Down                = -1
@@ -22,6 +25,7 @@ const (
 
 // Enum for the different types of buttons
 type ButtonType int
+
 const (
 	BT_HallUp   ButtonType = 0
 	BT_HallDown            = 1
@@ -50,8 +54,8 @@ func Init(addr string, numFloors int) {
 	_initialized = true
 }
 
-// Function that will need to be used 
-//Setting motor direction
+// Function that will need to be used when the elevator starts getting orders:
+// Setting motor direction
 func SetMotorDirection(dir MotorDirection) {
 	write([4]byte{1, byte(dir), 0, 0})
 }
@@ -61,18 +65,22 @@ func SetButtonLamp(button ButtonType, floor int, value bool) {
 	write([4]byte{2, byte(button), byte(floor), toByte(value)})
 }
 
+// Setting the floor indicator (shows which floor we are on)
 func SetFloorIndicator(floor int) {
 	write([4]byte{3, byte(floor), 0, 0})
 }
 
+// Setting the door open lamp
 func SetDoorOpenLamp(value bool) {
 	write([4]byte{4, toByte(value), 0, 0})
 }
 
+// Setting the stop lamp
 func SetStopLamp(value bool) {
 	write([4]byte{5, toByte(value), 0, 0})
 }
 
+// Checks if any of the buttons has been pressed
 func PollButtons(receiver chan<- ButtonEvent) {
 	prev := make([][3]bool, _numFloors)
 	for {
@@ -89,6 +97,7 @@ func PollButtons(receiver chan<- ButtonEvent) {
 	}
 }
 
+// Checks if the floor sensor registers any floors through "GetFloor()"
 func PollFloorSensor(receiver chan<- int) {
 	prev := -1
 	for {
@@ -101,6 +110,7 @@ func PollFloorSensor(receiver chan<- int) {
 	}
 }
 
+// Checks if the stop button has been pressed
 func PollStopButton(receiver chan<- bool) {
 	prev := false
 	for {
@@ -113,6 +123,7 @@ func PollStopButton(receiver chan<- bool) {
 	}
 }
 
+// Checks if the obstruction switch has been turned on
 func PollObstructionSwitch(receiver chan<- bool) {
 	prev := false
 	for {
@@ -125,11 +136,13 @@ func PollObstructionSwitch(receiver chan<- bool) {
 	}
 }
 
+// Function that returns if a button on a specific floor has been pushed (bool)
 func GetButton(button ButtonType, floor int) bool {
 	a := read([4]byte{6, byte(button), byte(floor), 0})
 	return toBool(a[1])
 }
 
+// Function that returns the floor the elevator is currently on
 func GetFloor() int {
 	a := read([4]byte{7, 0, 0, 0})
 	if a[1] != 0 {
@@ -139,39 +152,50 @@ func GetFloor() int {
 	}
 }
 
+// Function that returns if the stop button has been pushed (bool)
 func GetStop() bool {
 	a := read([4]byte{8, 0, 0, 0})
 	return toBool(a[1])
 }
 
+// Function that returns if the obstruction switch has been turned on (bool)
 func GetObstruction() bool {
 	a := read([4]byte{9, 0, 0, 0})
 	return toBool(a[1])
 }
 
+
+// Reads data from a connection (with mutex)
 func read(in [4]byte) [4]byte {
 	_mtx.Lock()
 	defer _mtx.Unlock()
-	
+
 	_, err := _conn.Write(in[:])
-	if err != nil { panic("Lost connection to Elevator Server") }
-	
+	if err != nil {
+		panic("Lost connection to Elevator Server")
+	}
+
 	var out [4]byte
 	_, err = _conn.Read(out[:])
-	if err != nil { panic("Lost connection to Elevator Server") }
-	
+	if err != nil {
+		panic("Lost connection to Elevator Server")
+	}
+
 	return out
 }
 
+// Writes data to a connection (with mutex)
 func write(in [4]byte) {
 	_mtx.Lock()
 	defer _mtx.Unlock()
-	
+
 	_, err := _conn.Write(in[:])
-	if err != nil { panic("Lost connection to Elevator Server") }
+	if err != nil {
+		panic("Lost connection to Elevator Server")
+	}
 }
 
-
+// Converts bool to byte
 func toByte(a bool) byte {
 	var b byte = 0
 	if a {
@@ -180,6 +204,7 @@ func toByte(a bool) byte {
 	return b
 }
 
+// Converts byte to bool
 func toBool(a byte) bool {
 	var b bool = false
 	if a != 0 {
