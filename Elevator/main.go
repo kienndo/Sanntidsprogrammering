499bff9ec7323e46c6d5ad746b679f9698439d7a
Kien Ninh Do <kiennd@stud.ntnu.no>
2024-03-06 16:41:09 +0100

    6.mars

--- Elevator/main.go
+++ Elevator/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"Sanntidsprogrammering/Elevator/devices"
 	"Sanntidsprogrammering/Elevator/elevio"
 	fsm "Sanntidsprogrammering/Elevator/fsm"
-	//"fmt"
+	timer "Sanntidsprogrammering/Elevator/timer"
+	"time"
+)
+var(
+	input devices.ElevInputDevice
 )
 
 func main() {
@@ -15,13 +20,43 @@ func main() {
 	fsm.FSM_init()
 
 	// Channels for all the different inputs
-	drv_buttons := make(chan elevio.ButtonEvent)
-	drv_floors := make(chan int)
-	drv_obstr := make(chan bool)
-	drv_stop := make(chan bool)
+	//drv_buttons := make(chan elevio.ButtonEvent)
+	//drv_floors := make(chan int)
+	//drv_obstr := make(chan bool)
+	//drv_stop := make(chan bool)
 
-	// Using state machine to run the elevator
+	input := devices.Elevio_GetInputDevice()
+    
+	if(input.FloorSensor() == -1){
+		fsm.FsmOnInitBetweenFloors();
+	}
+
 	for {
-	fsm.FSM_run(drv_buttons, drv_floors, drv_obstr, drv_stop, numFloors)
+		// Request button
+		var prevFloor [elevio.N_FLOORS][elevio.N_BUTTONS] int
+		for f := 0; f < elevio.N_FLOORS; f++ {
+			for b := 0; b < elevio.N_BUTTONS; b++ {
+				v := input.RequestButton(elevio.ButtonType(b), f)
+				if v != false && prevFloor[f][b] != v {
+					fsm.FsmOnRequestButtonPress(f, elevio.ButtonType(b))
+				}
+				prevFloor[f][b] = v
+			}
+		}
+		// Floor sensor
+		var prevFloor int = -1
+		f := input.FloorSensor()
+		if f != -1 && f != prevFloor {
+			fsm.FsmOnFloorArrival(f)
+		}
+		prevFloor = f
+
+		// Timer
+		if timer.TimerTimedOut() {
+			timer.TimerStop()
+			fsm.FsmOnDoorTimeout()
+		}
+
+		time.Sleep(time.Duration(25) * time.Millisecond)
 	}
 }
