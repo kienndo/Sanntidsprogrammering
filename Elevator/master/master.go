a0567a21b325dbd8c0d7c8cf8ecfde99fe9b48e1
Mona Lisa <MonaLisa@stud.ntnu.no>
2024-03-09 13:02:57 +0100

    Ingrid har startet å lage master

---
+++ Elevator/master/master.go
@@ -0,0 +1,13 @@
+package master
+
+import (
+	"fmt"
+	"elevio_io.go"
+	"os"
+)
+
+
+type master struct {
+	//elevators map[string][]
+}
+
---
+++ Elevator/udp/udp.go
@@ -0,0 +1,49 @@
+package udp
+
+import (
+	"encoding/gob"
+	"fmt"
+	"net"
+	"time"
+	
+)
+
+const StatePort = "20011"
+const DeletePort = "20012"
+const AlivePort = "20013"
+
+var State *net.UDPAddr
+var Delete *net.UDPAddr
+var Alive *net.UDPAddr
+
+func UDPInit() {
+	State, _ = net.ResolveUDPAddr("udp", ":" + StatePort)
+	Delete, _ = net.ResolveUDPAddr("udp", ":" + DeletePort)
+	Alive, _ = net.ResolveUDPAddr("udp", ":" + AlivePort)
+}
+
+func AliveSend() {
+
+	// Create socket
+	wrSocket, _ := net.DialUDP("udp", nil, Alive) 
+	defer wrSocket.Close()
+
+	// Create encoder
+	encoder := gob.NewEncoder(wrSocket)
+
+	message := "alive"
+
+	for {
+		//Encode and send
+		err := encoder.Encode(message)
+		if err != nil {
+			fmt.Println("Klarer ikke å sende melding",err)
+		}
+	}
+
+	time.Sleep(time.Second)
+
+
+}
+
+
