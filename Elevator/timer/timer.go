d953e84e447fc62b05ff0ce34b34dcf6b4b415d7
sirihelenew <sirihwa@stud.ntnu.no>
2024-03-13 16:08:06 +0100

    Commented code in timer.go

--- Elevator/timer/timer.go
+++ Elevator/timer/timer.go
@@ -9,17 +9,17 @@ var (
 	TimerEndTime float64
 	TimerActive  int
 )
-
+// Starts a timer with a given duration 
 func TimerStart(duration float64) {
 	TimerEndTime = float64(time.Now().Unix()) + duration
 	TimerActive = 1
 	fmt.Println("Timer started")
 }
-
+// Stops the timer. Used to ensure the door is open for a given DoorOpenDuration
 func TimerStop() {
 	TimerActive = 0
 }
-
+// Checks if the timer has timed out and returns 1 if it has
 func TimerTimedOut() int {
 	if TimerActive != 0 && float64(time.Now().Unix()) > TimerEndTime {
 		fmt.Println("Timed out")
